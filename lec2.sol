660f98af656423df586928e89dd50b136f4eabc0
alska95 <rhsalska55@naver.com>
2023-06-24 17:35:22 +0900

    remix solidity basic1

---
+++ lec2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0;
+
+contract lec2{
+    bool public b = false;
+
+    bytes4 public bt = 0x12345678;
+    bytes public vt2 = "string";
+
+    address public addr = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
+}
+
---
+++ lec3.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0;
+
+contract lec3{
+    //ethereum yello paper 참고
+
+    uint public value = 1 ether;
+    uint public value2 = 2 wei;
+    uint public value3 = 3 gwei;
+}
